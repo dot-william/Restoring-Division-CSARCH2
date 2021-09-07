351a110e4a5fb08319d2c31572610527138c9b08
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2021-09-07 21:33:05 +0800

    can now run app and show view

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const dotenv = require(`dotenv`);
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const path = require(`path`);
+const exphbs  = require('express-handlebars');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 dotenv.config();
 var port = process.env.PORT;
 var hostname = process.env.HOSTNAME;
 
-app.get(`/`, function (req, res) {
-    res.send("Hello world.");
+// Static Files
+app.use(express.static('public'));
+
+const hbs = exphbs.create({
+    extname: '.hbs'
 });
 
+app.engine('hbs', hbs.engine)
+app.set('view engine', '.hbs')
+
+app.get("/", function(req, res) {
+    res.render('restoring-division', {
+        layout:false
+    });
+});
 
 app.listen(port, hostname, function() {
     console.log("Server running at: ");
