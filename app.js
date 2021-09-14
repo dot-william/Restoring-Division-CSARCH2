95130bc6eed25af0ad164ca12bb5d9d074d51d3c
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2021-09-14 17:35:51 +0800

    linked up the other pages

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const bodyParser = require('body-parser');
 const path = require(`path`);
 const exphbs  = require('express-handlebars');
 
-const app = express();
+const app = express(); //express api
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//Setting up dotenv
 dotenv.config();
 var port = process.env.PORT;
 var hostname = process.env.HOSTNAME;
@@ -15,18 +16,41 @@ var hostname = process.env.HOSTNAME;
 // Static Files
 app.use(express.static('public'));
 
+//Setting up HBS
 const hbs = exphbs.create({
     extname: '.hbs'
 });
-
 app.engine('hbs', hbs.engine)
 app.set('view engine', '.hbs')
 
-app.get("/", function(req, res) {
+
+// Default home page
+app.get('/', function(req, res) {
     res.render('restoring-division', {
         layout:false
     });
 });
+
+// When user requests to go to step-by-step page 
+app.get('/step-by-step', function(req, res) {
+    console.log("this should appear");
+    res.render('step-by-step', {
+        layout:false
+    });
+});
+
+// When user requests for the home page
+app.get('/home', function(req, res) {
+    console.log("someone pressed home");
+    res.redirect('/'); //redirect to home page
+});
+
+
+//When user requests for  page that doesn't exist
+app.use(function(req, res) {
+    res.status(404).send("Invalid access, please return back to the home page");
+});
+
 
 app.listen(port, hostname, function() {
     console.log("Server running at: ");
