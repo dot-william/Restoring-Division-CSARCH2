5c9bd745d6abaf23d6b77c58228f7ad3cbc58c0f
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2021-09-16 19:11:32 +0800

    removed console log

--- public/js/algorithm.js
+++ public/js/algorithm.js
@@ -333,9 +333,6 @@ $(document).ready(function () {
         text +=" M : " + M + "\n\n";
 
         
-
-        
-        console.log("hellow");
         /* START LOOP HERE FOR STEP BY STEP */
         for (let i = 1; i <= Q.length; i++) {
           tempAQ = leftShifting(AQ, 1);
